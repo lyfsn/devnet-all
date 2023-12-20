043ffa0721752398ea9f551e290094a5ea7478fa
lyfsn <dev.wangyu@proton.me>
2023-12-20 11:44:17 +0800

    up

---
+++ geth-lighthouse-2/start.deva.sh
@@ -0,0 +1,13 @@
+
+
+
+
+export IP_ADDRESS=88.99.94.109
+export EL_BOOTNODES="enode://ef23ed298a1badc12ffe6d80e6777914c2d2c46fb2b913698906e8c573ab505d550c6ab55639bc3cf0802ce6d595dc5b410dee7c8fcfd08ecd3fc6d14cdf7497@78.46.91.61:50403"
+export CL_TRUSTED_PEERS=16Uiu2HAmByKAUsK8nAGeonQJcaSXR5pMZeuGpT33XGx6pPCi49zK
+export CL_BOOTNODES="enr:-MS4QA_qJU0cix0wl4Njxpy-zUWrPrxEE8yVc9FVPJQQRn0HdEPWlA_PK928iWWkb36rxq83T7BVQnR0L_xJz6DDX1hFh2F0dG5ldHOI__________-EZXRoMpDT1PoxQAAAOP__________gmlkgnY0gmlwhE4uWz2EcXVpY4IrXYlzZWNwMjU2azGhAvXWClCpRJfBewrXBuZg4UjwcA1dgBoGFTOcPnr6WAgEiHN5bmNuZXRzD4N0Y3CCK1yDdWRwgitc"
+
+docker compose -f compose.yaml up -d
+
+
+
