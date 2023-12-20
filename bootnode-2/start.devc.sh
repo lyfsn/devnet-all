03e0f08f891eb36198482e868094ed903543e842
lyfsn <dev.wangyu@proton.me>
2023-12-20 15:52:38 +0800

    up

---
+++ bootnode-2/start.devc.sh
@@ -0,0 +1,16 @@
+
+
+
+
+export IP_ADDRESS=168.119.5.82
+
+
+# bootnodes
+export EL_BOOTNODES="enode://128f63a201e11e5079a8e8ce4ec30377adbfbbf1731824de51b2cf2c76a1426c122ef2e3e543b79ab9da958ae23c3cbb3272d3fe55e3e3541f0497a9d400cc8e@78.46.91.61:30303"
+export CL_BOOTNODES="enr:-L24QFLX-mOcx-wzj--4-oOqXX0e33Q8fr_ITkyVhKdSjyf-H9mvZRrmbj0HcA-YtaXjoTE6EImPdcyVjg3qUecztyILh2F0dG5ldHOIAAAAAAAAAACEZXRoMpDT1PoxQAAAOP__________gmlkgnY0gmlwhE4uWz2EcXVpY4IjKYlzZWNwMjU2azGhA4LWSZsyrFITnQgWHSF3GpVjBjKJQYe9TJQkXoGyvXuciHN5bmNuZXRzAIN0Y3CCIyg"
+
+
+docker compose -f compose.yaml up -d
+
+
+
