08401e77c731de742ec57ee29e531546e8cd8755
lyfsn <dev.wangyu@proton.me>
2023-12-21 19:53:16 +0800

    up

--- p2ptest-3/startBeacon.sh
+++ p2ptest-3/startBeacon.sh
@@ -2,7 +2,6 @@
   --debug-level=info \
   --datadir=./consensus-data \
   --testnet-dir=./el-cl-genesis-data/custom_config_data \
-  --disable-enr-auto-update \
   --enr-address=78.46.91.61 \
   --enr-udp-port=11031 \
   --enr-tcp-port=11031 \
