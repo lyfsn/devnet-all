88cd1e2326d9ccc10f8161d9a811353096d9f931
lyfsn <dev.wangyu@proton.me>
2023-12-22 13:00:49 +0800

    up

---
+++ p2ptest-3/startBeacon4.sh
@@ -0,0 +1,21 @@
+/root/.cargo/bin/lighthouse bn \
+  --debug-level=info \
+  --datadir=./consensus-data \
+  --testnet-dir=./el-cl-genesis-data/custom_config_data \
+  --listen-address=0.0.0.0 \
+  --http \
+  --http-address=0.0.0.0 \
+  --http-allow-sync-stalled \
+  --slots-per-restore-point=32 \
+  --disable-packet-filter \
+  --execution-endpoints=http://127.0.0.1:8551 \
+  --jwt-secrets=./el-cl-genesis-data/jwt/jwtsecret \
+  --suggested-fee-recipient=0x8943545177806ED17B9F23F0a21ee5948eCaa776 \
+  --subscribe-all-subnets \
+  --import-all-attestations \
+  --metrics \
+  --metrics-address=0.0.0.0 \
+  --metrics-allow-origin=* \
+  --target-peers=16 \
+  --disable-peer-scoring \
+  
---
+++ p2ptest/startGeth4.sh
@@ -0,0 +1,24 @@
+
+./geth \
+ --networkid=38356 \
+ --state.scheme=path \
+ --verbosity=3 \
+ --datadir=./execution-data \
+ --http \
+ --http.addr=0.0.0.0 \
+ --http.vhosts=* \
+ --http.corsdomain=* \
+ --http.api=admin,engine,net,eth,web3,debug \
+ --ws \
+ --ws.addr=0.0.0.0 \
+ --ws.api=admin,engine,net,eth,web3,debug \
+ --ws.origins=* \
+ --allow-insecure-unlock \
+ --verbosity=3 \
+ --authrpc.addr=0.0.0.0 \
+ --authrpc.vhosts=* \
+ --authrpc.jwtsecret=./el-cl-genesis-data/jwt/jwtsecret \
+ --syncmode=full \
+ --rpc.allow-unprotected-txs \
+ --metrics \
+ --metrics.addr=0.0.0.0 \
