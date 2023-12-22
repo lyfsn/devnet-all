896b75bf7bf2404f74a60cae6dc4a302e5aeea77
lyfsn <dev.wangyu@proton.me>
2023-12-22 15:42:20 +0800

    up

--- p2ptest-3/startBeacon.sh
+++ p2ptest-3/startBeacon.sh
@@ -21,7 +21,7 @@
   --metrics-port=5184 \
   --enable-private-discovery=true \
   --target-peers=16 \
-  --boot-nodes=enr:-MS4QIJEVR4oQhwu8acaVhMA4EPbXK_dEUHlpd_A90NdlH-4acyWn_yAwc6jAQtvQPr0kCyhtW6iYit1IFyC_0Kb9pEBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpDT1PoxQAAAOP__________gmlkgnY0gmlwhH8AAAGEcXVpY4Iq-YlzZWNwMjU2azGhAsJm92CYcaHldTJM5guxl9dfM2GFWkjG4dFth8ObVkkBiHN5bmNuZXRzAIN0Y3CCKvmDdWRwgir5 \
+  --boot-nodes=enr:-MS4QDuy1K75IAcq2WhtWYErmj0-tM4T_yw5F_TcC5jhzD42Bax_VTa8axmCfi016tzdbIEc3yz3h6fNj_w2ombfAexGh2F0dG5ldHOIAAAAAAAAAACEZXRoMpDT1PoxQAAAOP__________gmlkgnY0gmlwhH8AAAGEcXVpY4Iq-YlzZWNwMjU2azGhAsJm92CYcaHldTJM5guxl9dfM2GFWkjG4dFth8ObVkkBiHN5bmNuZXRzAIN0Y3CCKviDdWRwgir4 \
   --enr-address=127.0.0.1  \
   --disable-peer-scoring \
   --enr-udp-port=11031 \
