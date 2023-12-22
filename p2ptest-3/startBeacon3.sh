770352ac59974d417c64a10723437654dfe748ef
lyfsn <dev.wangyu@proton.me>
2023-12-22 12:51:57 +0800

    up

---
+++ p2ptest-3/startBeacon3.sh
@@ -0,0 +1,30 @@
+/root/.cargo/bin/lighthouse bn \
+  --debug-level=trace \
+  --datadir=./consensus-data \
+  --testnet-dir=./el-cl-genesis-data/custom_config_data \
+  --listen-address=0.0.0.0 \
+  --port=11031 \
+  --http \
+  --http-address=0.0.0.0 \
+  --http-port=5131 \
+  --http-allow-sync-stalled \
+  --slots-per-restore-point=32 \
+  --disable-packet-filter \
+  --execution-endpoints=http://127.0.0.1:8572 \
+  --jwt-secrets=./el-cl-genesis-data/jwt/jwtsecret \
+  --suggested-fee-recipient=0x8943545177806ED17B9F23F0a21ee5948eCaa776 \
+  --subscribe-all-subnets \
+  --import-all-attestations \
+  --metrics \
+  --metrics-address=0.0.0.0 \
+  --metrics-allow-origin=* \
+  --metrics-port=5184 \
+  --enable-private-discovery=true \
+  --target-peers=16 \
+  --boot-nodes=enr:-MS4QDB8fJC6q1t0mUD9hdAExwdqC_M3nwjoRpMOOE6DR8traY0f5uc8DPXFL96NAjYFI8JoMoEcGNa_waWm3RxizS8Ch2F0dG5ldHOIAAAAAAAAAACEZXRoMpDT1PoxQAAAOP__________gmlkgnY0gmlwhJUcMOCEcXVpY4Iq-YlzZWNwMjU2azGhA1RnFOwLma7Z8MZBl7aOTlVTK1wwrUSt8wYYzbty9rebiHN5bmNuZXRzAIN0Y3CCKvmDdWRwgir5 \
+  --enr-address=127.0.0.1 \
+  --disable-peer-scoring \
+  --enr-udp-port=11032 \
+  --enr-tcp-port=11032 \
+
+
---
+++ p2ptest-3/startGeth3.sh
@@ -0,0 +1,32 @@
+
+./geth \
+ --networkid=38356 \
+ --state.scheme=path \
+ --verbosity=3 \
+ --datadir=./execution-data \
+ --http \
+ --http.addr=0.0.0.0 \
+ --http.vhosts=* \
+ --http.corsdomain=* \
+ --http.api=admin,engine,net,eth,web3,debug \
+ --http.port=8565 \
+ --ws \
+ --ws.addr=0.0.0.0 \
+ --ws.port=8566 \
+ --ws.api=admin,engine,net,eth,web3,debug \
+ --ws.origins=* \
+ --allow-insecure-unlock \
+ --verbosity=3 \
+ --authrpc.port=8572 \
+ --authrpc.addr=0.0.0.0 \
+ --authrpc.vhosts=* \
+ --authrpc.jwtsecret=./el-cl-genesis-data/jwt/jwtsecret \
+ --syncmode=full \
+ --rpc.allow-unprotected-txs \
+ --metrics \
+ --metrics.addr=0.0.0.0 \
+ --metrics.port=9102 \
+ --port=50305 \
+ --discovery.port=50305 \
+ --bootnodes=enode://a026723171b4f8eb7f98b3192d8ae8fc3905303e838622c3e0c793a904196c53b3f7f4f5e2a99ad598259519a7406a7a82cfb7f90473970e1f560280001ede8a@149.28.48.224:50303 \
+ --nat=extip:127.0.0.1
